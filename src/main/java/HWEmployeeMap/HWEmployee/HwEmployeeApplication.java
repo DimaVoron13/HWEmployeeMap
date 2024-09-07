718abe79e640f0e40e2f6728af0a352a95bc1af6
Дмитрий <dima.voron13666@gmail.com>
2024-09-07 03:05:18 +0300

    111

---
+++ src/main/java/HWEmployeeMap/HWEmployee/HwEmployeeApplication.java
@@ -0,0 +1,13 @@
+package HWEmployeeMap.HWEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwEmployeeApplication.class, args);
+	}
+
+}
